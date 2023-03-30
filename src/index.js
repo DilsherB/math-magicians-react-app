7d8a8bf050f1670ebd5c3c0c25dd7889251cea46
Dilsher Balouch <dilsherb@gmail.com>
2023-03-30 19:56:07 +0300

    file ext. added in import statement in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Calculator from './components/Calculator';
+import Calculator from './components/Calculator.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
