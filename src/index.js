17ee7b516fef53d4377a8756359c656fc0e40310
Dilsher Balouch <dilsherb@gmail.com>
2023-03-30 20:07:08 +0300

    ext. removed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Calculator from './components/Calculator.jsx';
+import Calculator from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
